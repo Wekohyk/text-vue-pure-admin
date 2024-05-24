d52a4509115c2594b6f51a27e3daba0fd6d4dc37
huangyukai <wekohyk@gmail.com>
2024-05-24 16:01:31 +0800

    feat: Update language selection to use localStorage for persistence

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,18 +1,17 @@
 // localStorage.getItem('lang');
 import { createI18n } from 'vue-i18n';
-import { getLocalStorage } from '@/utils/localStorage';
 import zh from './zh';
 import en from './en';
 
 // judge if it is English
 export const isEn =
-  getLocalStorage('text-vue-pure-admin-lang') === 'en' ? true : false;
+  localStorage.getItem('text-vue-pure-admin-lang') === 'en' ? true : false;
 
 // restraint all lang object to typeof zh
 const i18n = createI18n({
   legacy: false,
   globalInjection: true,
-  locale: getLocalStorage('text-vue-pure-admin-lang') || 'en',
+  locale: localStorage.getItem('text-vue-pure-admin-lang') || 'en',
   messages: {
     zh,
     en,
