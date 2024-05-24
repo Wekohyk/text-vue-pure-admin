2f01474539d6196ad7768f605546f212d304e7c3
huangyukai <wekohyk@gmail.com>
2024-05-24 16:17:22 +0800

    feat: Add pagination to Dashboard component

--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -32,6 +32,9 @@
   <div>123</div>
   <div>123</div>
   <div>123</div>
+  <div class="flex justify-center my-10">
+    <el-pagination background layout="prev, pager, next" :total="1000" />
+  </div>
 </template>
 <script setup lang="ts"></script>
 <style scoped lang="scss"></style>
