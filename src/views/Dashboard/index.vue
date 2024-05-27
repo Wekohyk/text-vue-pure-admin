2d4166172ecdf6a53d6658aca7b012efd2a30fb1
huangyukai <wekohyk@gmail.com>
2024-05-27 11:39:34 +0800

    feat: Update Dashboard component to use new ECharts instance and options

--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div id="echarts" style="width: 40vw; height: 40vh"></div>
+    <div id="echarts1" style="width: 40vw; height: 40vh"></div>
   </div>
   <!-- <div class="flex justify-center my-10">
     <el-pagination background layout="prev, pager, next" :total="1000" />
@@ -11,39 +11,24 @@
 import * as echarts from 'echarts';
 import { onMounted, ref, onUnmounted } from 'vue';
 import { ECharts as EChartsType } from 'echarts';
+import { option1 } from './data/echartsData';
 
-const myChart = ref<EChartsType | null>(null);
+const myChart1 = ref<EChartsType | null>(null);
 
 let resizeHandler: () => void;
 
 onMounted(() => {
   // 基于准备好的dom，初始化echarts实例
-  myChart.value = echarts.init(document.getElementById('echarts'));
+  myChart1.value = echarts.init(document.getElementById('echarts1'));
   // 绘制图表
-  const option = {
-    title: {
-      text: 'ECharts 入门示例',
-    },
-    tooltip: {},
-    xAxis: {
-      data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
-    },
-    yAxis: {},
-    series: [
-      {
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20],
-      },
-    ],
-  };
+  const option = option1;
 
-  myChart.value.setOption(option);
+  myChart1.value.setOption(option);
 
   // 创建一个调整图表大小的函数
   resizeHandler = () => {
-    if (myChart.value) {
-      myChart.value.resize();
+    if (myChart1.value) {
+      myChart1.value.resize();
     }
   };
 
